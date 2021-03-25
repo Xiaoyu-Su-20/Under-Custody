0e5cfd1c9883d5f3d6e2e68c2b7b328a7b19fde7
aulichney <annie.ulichney@yale.edu>
2021-03-25 10:34:26 -0400

    table and number format changes

--- bar.js
+++ bar.js
@@ -5,24 +5,29 @@ import { select, axisBottom, axisLeft } from "d3";
 import { transformData } from "./useData";
 
 
-// bar constants 
+// bar constants
 const WIDTH = 600;
 const HEIGHT= 400;
 const margin={top: 25, right: 25, bottom: 50, left: 80};
 const innerWidth = WIDTH - margin.left - margin.right;
 const innerHeight = HEIGHT - margin.top - margin.bottom;
 
+//Styling and format functions:
 //Title case function for axis title formatting
 function toTitle(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+//Number formatting
+function formatNumber(num) {
+  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+}
 
 
 //sort constant, 'none'; 'height': sort by height descendant; 'x': sort by x value
-let sort_status = 'none'; 
+let sort_status = 'none';
 const SORT_DURATION = 500;
 
-// create the svg object 
+// create the svg object
 const SVG = (ref) => {
   // the temporary solution is this, prevent react from appending svgs indefinitely
   	if (d3.selectAll("svg").empty()) {
@@ -39,7 +44,7 @@ const Bar = (ref_radio, barData, yAttribute, xAttribute) => {
 		const barAdjust = 5 / barData.length // for adjusting the width of bars
     const svg = d3.select("svg")
     // remove everything from svg and rerender objects
-    svg.selectAll("*").remove();  
+    svg.selectAll("*").remove();
 
     const xScale = d3.scaleBand()
                .domain(barData.map(d => d.key))
@@ -48,7 +53,7 @@ const Bar = (ref_radio, barData, yAttribute, xAttribute) => {
     const yScale = d3.scaleLinear()
                    .domain([0, d3.max( barData.map(d => d.value[yAttribute]) )] )
                    .range([innerHeight, 0]).nice();
-		
+
     //--------------------------------------------------------------------------------
     // draw initial bars
     const bars = svg.append('g')
@@ -65,8 +70,7 @@ const Bar = (ref_radio, barData, yAttribute, xAttribute) => {
           tooltip
             .html(
               `<div>${toTitle(xAttribute)}: ${d.key}</div>
-              <div>${toTitle(yAttribute)}: ${d.value[yAttribute].toFixed(2)}</div>
-              <div>Percent: ${(d.value[yAttribute]/totalPop*100).toFixed(2)}%</div>`
+              <div>${toTitle(yAttribute)}: ${formatNumber(d.value[yAttribute].toFixed(2))}</div>`
             )
             .style('visibility', 'visible');
           d3.select(this).style("opacity", 1);
@@ -80,8 +84,8 @@ const Bar = (ref_radio, barData, yAttribute, xAttribute) => {
           tooltip.html(``).style('visibility', 'hidden');
           d3.select(this).style("opacity", 0.7);
       });
-  
-  	
+
+
     //moueover tooltip
     const totalPop = d3.sum(barData.map((d) => d.value[yAttribute])); //counts number of individuals in custody
     const tooltip = d3
@@ -96,9 +100,9 @@ const Bar = (ref_radio, barData, yAttribute, xAttribute) => {
                     .style('border-radius', '4px')
                     .style('color', '#fff')
                     .text('a simple tooltip');
-  
-  
-  
+
+
+
   	//--------------------------------------------------------------------------------
     // draw axes
 
@@ -115,7 +119,7 @@ const Bar = (ref_radio, barData, yAttribute, xAttribute) => {
       .attr("class", "axis")
       .attr("transform", `translate (${margin.left}, ${margin.top})`)
       .call(yAxis);
-  	
+
   	//--------------------------------------------------------------------------------
     //Axis labels
     svg
@@ -133,17 +137,17 @@ const Bar = (ref_radio, barData, yAttribute, xAttribute) => {
       .attr("x", 0 + WIDTH/2)
       .attr("dy", "1.5em")
       .text(toTitle(xAttribute));
-  	
+
  	 	//--------------------------------------------------------------------------------
-  	// sorting 
-  	// radio button calls sort function on click 
+  	// sorting
+  	// radio button calls sort function on click
   	d3.select(ref_radio)
     .selectAll("input")
     .on("click", sort)
-		
+
     // sort when changing dropdown menu given the sorted button is already selected
     sort(sort_status)
-  	
+
 
     function change_data(new_data, duration, delay=0) {
       //change the axis generator
@@ -160,16 +164,16 @@ const Bar = (ref_radio, barData, yAttribute, xAttribute) => {
             .attr("width", xScale.bandwidth()-barAdjust*2)
             .attr("height", d => innerHeight - yScale(d.value[yAttribute]))
     };
-  
+
     // argument is optional, used when changing dropdown menu given the sorted button is already selected
-    function sort(arg) {   
+    function sort(arg) {
 
       if (typeof arg == 'string') { // when changing dropdown menu given the sorted button is already selected
         var action = arg
         var duration = 0
-      } else { // when no argument is passed into sort, get value from the radio button 
+      } else { // when no argument is passed into sort, get value from the radio button
         var action = d3.select(this).node().value;
-        var duration = SORT_DURATION;  
+        var duration = SORT_DURATION;
       }
       // console.log(action)
 
@@ -178,8 +182,8 @@ const Bar = (ref_radio, barData, yAttribute, xAttribute) => {
         change_data(new_data, duration);
         sort_status = 'height';
       } else if (action == 'x') {
-        // if the str is a number, compare the number, not the strings. If we can process the 
-        // data so that the key remains numeric data type in the transform function, we don't need this step       
+        // if the str is a number, compare the number, not the strings. If we can process the
+        // data so that the key remains numeric data type in the transform function, we don't need this step
         if (barData[0].key.match("\\d+")) {
           var new_data = barData.slice().sort((a,b) => d3.ascending(parseInt(a.key), parseInt(b.key)));
         } else {
@@ -187,9 +191,9 @@ const Bar = (ref_radio, barData, yAttribute, xAttribute) => {
         }
         change_data(new_data, duration)
         sort_status = 'x';
-      }  
+      }
     };
-  
+
 };
 
 //Table
@@ -243,30 +247,30 @@ const Table = ({ barData, yAttribute, xAttribute}) => {
       row1.push(<tr key={i} id={rowID}>{cell}</tr>)
     };
 
-  //Fill table by column. Col 1 is each category for the given xattribute. Col 2 is the value for each category.
-  //Col 3 is percent of total population for each category
-  for (var i = 1; i < xLength + 1; i++){
-      let rowID = `row${i}`
-      let cell = []
-      for (var idx = 0; idx < 1; idx++){
-        let cellID = `cell${i}-${idx}`
-        let entry = xScale.domain()[i-1]
-        cell.push(<td key={cellID} id={cellID}>{entry}</td>)
-      }
-    	for (var idx = 1; idx < 2; idx++){
-        let cellID = `cell${i}-${idx}`
-        let entry = count[i-1].toFixed(0)
-        cell.push(<td key={cellID} id={cellID}>{entry}</td>)
-      }
-    	if(yAttribute == 'amount'){
-        for (var idx = 2; idx < 3; idx++){
+    //Fill table by column. Col 1 is each category for the given xattribute. Col 2 is the value for each category.
+    //Col 3 is percent of total population for each category
+    for (var i = 1; i < xLength + 1; i++){
+        let rowID = `row${i}`
+        let cell = []
+        for (var idx = 0; idx < 1; idx++){
           let cellID = `cell${i}-${idx}`
-          let entry = pct[i-1].toFixed(2)
-          cell.push(<td key={cellID} id={cellID}>{entry}%</td>)
+          let entry = xScale.domain()[i-1]
+          cell.push(<td key={cellID} id={cellID}>{entry}</td>)
         }
-      }
-      rows.push(<tr key={i} id={rowID}>{cell}</tr>)
-    };
+      	for (var idx = 1; idx < 2; idx++){
+          let cellID = `cell${i}-${idx}`
+          let entry = count[i-1].toFixed(0)
+          cell.push(<td key={cellID} id={cellID}>{formatNumber(entry)}</td>)
+        }
+      	if(yAttribute == 'amount'){
+          for (var idx = 2; idx < 3; idx++){
+            let cellID = `cell${i}-${idx}`
+            let entry = pct[i-1].toFixed(2)
+            cell.push(<td key={cellID} id={cellID}>{entry}%</td>)
+          }
+        }
+        rows.push(<tr key={i} id={rowID}>{cell}</tr>)
+      };
 
 
 
@@ -293,24 +297,24 @@ const Table = ({ barData, yAttribute, xAttribute}) => {
 };
 
 export const Chart = ( {rawData} ) => {
-  
-  // create React hooks for controlling the grouped data we want to generate; also, setup the initial value 
+
+  // create React hooks for controlling the grouped data we want to generate; also, setup the initial value
   const [xAttribute, setXAttribute] = useState('sex');
   const [yAttribute, setYAttribute] = useState('amount');
-  
+
   // according to the current xAttr ibute, group by that attribute and compute the number of observations and the average age
   const barData = transformData(rawData, xAttribute)
-  
+
   console.log(barData)
 
-  // map each column to { value: col, label: col } to feed into react Dropdown menu 
+  // map each column to { value: col, label: col } to feed into react Dropdown menu
   const xFields = Object.keys(rawData[0]).map(d => ({"value":d, "label":d}));
-  
+
   console.log(xFields)
 
   const yFields = Object.keys(barData[0].value).map(d => ({"value":d, "label":d}));
 
-  // return the title, the dropdown menus, and the barplot with axes  
+  // return the title, the dropdown menus, and the barplot with axes
 	return(
     <>
       <header>
@@ -319,9 +323,9 @@ export const Chart = ( {rawData} ) => {
       </div>
       <h1> NYDOCCS Under Custody Data </h1>
 			</header>
-      
+
       <h1 ref={d => SVG(d)}> </h1>
-			
+
       <div className='menu-container'>
       <span className="dropdown-label">X</span>
       <ReactDropdown
@@ -336,23 +340,22 @@ export const Chart = ( {rawData} ) => {
         onChange={({value, label}) => setYAttribute(value)}
       />
       </div>
-      
+
 			<div id='radio_sort' ref={d => Bar(d, barData, yAttribute, xAttribute)} class="control-group">
         <label class="control control-radio">
             Sort by Height
-            <input  className='radio' type="radio" value="height" name="sort" /> 
+            <input  className='radio' type="radio" value="height" name="sort" />
             <div class="control_indicator"></div>
         </label>
         <label class="control control-radio">
-            Sort by X Value 
-            <input className='radio' type="radio" value="x" name="sort" /> 
+            Sort by X Value
+            <input className='radio' type="radio" value="x" name="sort" />
             <div class="control_indicator"></div>
         </label>
     </div>
-      
-      
+
+
     <Table barData={barData} yAttribute={yAttribute} xAttribute = {xAttribute}/>
 		</>
 	);
 };
-
--- bundle.js
+++ bundle.js
@@ -56,24 +56,29 @@
     return data;
   };
 
-  // bar constants 
+  // bar constants
   var WIDTH = 600;
   var HEIGHT= 400;
   var margin={top: 25, right: 25, bottom: 50, left: 80};
   var innerWidth = WIDTH - margin.left - margin.right;
   var innerHeight = HEIGHT - margin.top - margin.bottom;
 
+  //Styling and format functions:
   //Title case function for axis title formatting
   function toTitle(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
+  //Number formatting
+  function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+  }
 
 
   //sort constant, 'none'; 'height': sort by height descendant; 'x': sort by x value
-  var sort_status = 'none'; 
+  var sort_status = 'none';
   var SORT_DURATION = 500;
 
-  // create the svg object 
+  // create the svg object
   var SVG = function (ref) {
     // the temporary solution is this, prevent react from appending svgs indefinitely
     	if (d3.selectAll("svg").empty()) {
@@ -90,7 +95,7 @@
   		var barAdjust = 5 / barData.length; // for adjusting the width of bars
       var svg = d3.select("svg");
       // remove everything from svg and rerender objects
-      svg.selectAll("*").remove();  
+      svg.selectAll("*").remove();
 
       var xScale = d3.scaleBand()
                  .domain(barData.map(function (d) { return d.key; }))
@@ -99,7 +104,7 @@
       var yScale = d3.scaleLinear()
                      .domain([0, d3.max( barData.map(function (d) { return d.value[yAttribute]; }) )] )
                      .range([innerHeight, 0]).nice();
-  		
+
       //--------------------------------------------------------------------------------
       // draw initial bars
       var bars = svg.append('g')
@@ -115,7 +120,7 @@
     		.on('mouseover', function (d, i) {
             tooltip
               .html(
-                ("<div>" + (toTitle(xAttribute)) + ": " + (d.key) + "</div>\n              <div>" + (toTitle(yAttribute)) + ": " + (d.value[yAttribute].toFixed(2)) + "</div>\n              <div>Percent: " + ((d.value[yAttribute]/totalPop*100).toFixed(2)) + "%</div>")
+                ("<div>" + (toTitle(xAttribute)) + ": " + (d.key) + "</div>\n              <div>" + (toTitle(yAttribute)) + ": " + (formatNumber(d.value[yAttribute].toFixed(2))) + "</div>")
               )
               .style('visibility', 'visible');
             d3.select(this).style("opacity", 1);
@@ -129,10 +134,10 @@
             tooltip.html("").style('visibility', 'hidden');
             d3.select(this).style("opacity", 0.7);
         });
-    
-    	
+
+
       //moueover tooltip
-      var totalPop = d3.sum(barData.map(function (d) { return d.value[yAttribute]; })); //counts number of individuals in custody
+      d3.sum(barData.map(function (d) { return d.value[yAttribute]; })); //counts number of individuals in custody
       var tooltip = d3
                       .select('body')
                       .append('div')
@@ -145,9 +150,9 @@
                       .style('border-radius', '4px')
                       .style('color', '#fff')
                       .text('a simple tooltip');
-    
-    
-    
+
+
+
     	//--------------------------------------------------------------------------------
       // draw axes
 
@@ -164,7 +169,7 @@
         .attr("class", "axis")
         .attr("transform", ("translate (" + (margin.left) + ", " + (margin.top) + ")"))
         .call(yAxis);
-    	
+
     	//--------------------------------------------------------------------------------
       //Axis labels
       svg
@@ -182,17 +187,17 @@
         .attr("x", 0 + WIDTH/2)
         .attr("dy", "1.5em")
         .text(toTitle(xAttribute));
-    	
+
    	 	//--------------------------------------------------------------------------------
-    	// sorting 
-    	// radio button calls sort function on click 
+    	// sorting
+    	// radio button calls sort function on click
     	d3.select(ref_radio)
       .selectAll("input")
       .on("click", sort);
-  		
+
       // sort when changing dropdown menu given the sorted button is already selected
       sort(sort_status);
-    	
+
 
       function change_data(new_data, duration, delay) {
         if ( delay === void 0 ) delay=0;
@@ -210,16 +215,16 @@
               .attr("y", function (d) { return yScale(d.value[yAttribute]); })
               .attr("width", xScale.bandwidth()-barAdjust*2)
               .attr("height", function (d) { return innerHeight - yScale(d.value[yAttribute]); });
-      }  
+      }
       // argument is optional, used when changing dropdown menu given the sorted button is already selected
-      function sort(arg) {   
+      function sort(arg) {
 
         if (typeof arg == 'string') { // when changing dropdown menu given the sorted button is already selected
           var action = arg;
           var duration = 0;
-        } else { // when no argument is passed into sort, get value from the radio button 
+        } else { // when no argument is passed into sort, get value from the radio button
           var action = d3.select(this).node().value;
-          var duration = SORT_DURATION;  
+          var duration = SORT_DURATION;
         }
         // console.log(action)
 
@@ -228,8 +233,8 @@
           change_data(new_data$1, duration);
           sort_status = 'height';
         } else if (action == 'x') {
-          // if the str is a number, compare the number, not the strings. If we can process the 
-          // data so that the key remains numeric data type in the transform function, we don't need this step       
+          // if the str is a number, compare the number, not the strings. If we can process the
+          // data so that the key remains numeric data type in the transform function, we don't need this step
           if (barData[0].key.match("\\d+")) {
             var new_data = barData.slice().sort(function (a,b) { return d3.ascending(parseInt(a.key), parseInt(b.key)); });
           } else {
@@ -237,8 +242,8 @@
           }
           change_data(new_data, duration);
           sort_status = 'x';
-        }  
-      }  
+        }
+      }
   };
 
   //Table
@@ -295,30 +300,30 @@
         }
         row1.push(React.createElement( 'tr', { key: i, id: rowID }, cell));
       }
-    //Fill table by column. Col 1 is each category for the given xattribute. Col 2 is the value for each category.
-    //Col 3 is percent of total population for each category
-    for (var i = 1; i < xLength + 1; i++){
-        var rowID$1 = "row" + i;
-        var cell$1 = [];
-        for (var idx = 0; idx < 1; idx++){
-          var cellID$4 = "cell" + i + "-" + idx;
-          var entry = xScale.domain()[i-1];
-          cell$1.push(React.createElement( 'td', { key: cellID$4, id: cellID$4 }, entry));
-        }
-      	for (var idx = 1; idx < 2; idx++){
-          var cellID$5 = "cell" + i + "-" + idx;
-          var entry$1 = count[i-1].toFixed(0);
-          cell$1.push(React.createElement( 'td', { key: cellID$5, id: cellID$5 }, entry$1));
-        }
-      	if(yAttribute == 'amount'){
-          for (var idx = 2; idx < 3; idx++){
-            var cellID$6 = "cell" + i + "-" + idx;
-            var entry$2 = pct[i-1].toFixed(2);
-            cell$1.push(React.createElement( 'td', { key: cellID$6, id: cellID$6 }, entry$2, "%"));
+      //Fill table by column. Col 1 is each category for the given xattribute. Col 2 is the value for each category.
+      //Col 3 is percent of total population for each category
+      for (var i = 1; i < xLength + 1; i++){
+          var rowID$1 = "row" + i;
+          var cell$1 = [];
+          for (var idx = 0; idx < 1; idx++){
+            var cellID$4 = "cell" + i + "-" + idx;
+            var entry = xScale.domain()[i-1];
+            cell$1.push(React.createElement( 'td', { key: cellID$4, id: cellID$4 }, entry));
           }
+        	for (var idx = 1; idx < 2; idx++){
+            var cellID$5 = "cell" + i + "-" + idx;
+            var entry$1 = count[i-1].toFixed(0);
+            cell$1.push(React.createElement( 'td', { key: cellID$5, id: cellID$5 }, formatNumber(entry$1)));
+          }
+        	if(yAttribute == 'amount'){
+            for (var idx = 2; idx < 3; idx++){
+              var cellID$6 = "cell" + i + "-" + idx;
+              var entry$2 = pct[i-1].toFixed(2);
+              cell$1.push(React.createElement( 'td', { key: cellID$6, id: cellID$6 }, entry$2, "%"));
+            }
+          }
+          rows.push(React.createElement( 'tr', { key: i, id: rowID$1 }, cell$1));
         }
-        rows.push(React.createElement( 'tr', { key: i, id: rowID$1 }, cell$1));
-      }
 
 
     //create table element with rows
@@ -346,28 +351,28 @@
   var Chart = function ( ref ) {
     var rawData = ref.rawData;
 
-    
-    // create React hooks for controlling the grouped data we want to generate; also, setup the initial value 
+
+    // create React hooks for controlling the grouped data we want to generate; also, setup the initial value
     var ref$1 = React$1.useState('sex');
     var xAttribute = ref$1[0];
     var setXAttribute = ref$1[1];
     var ref$2 = React$1.useState('amount');
     var yAttribute = ref$2[0];
     var setYAttribute = ref$2[1];
-    
+
     // according to the current xAttr ibute, group by that attribute and compute the number of observations and the average age
     var barData = transformData(rawData, xAttribute);
-    
+
     console.log(barData);
 
-    // map each column to { value: col, label: col } to feed into react Dropdown menu 
+    // map each column to { value: col, label: col } to feed into react Dropdown menu
     var xFields = Object.keys(rawData[0]).map(function (d) { return ({"value":d, "label":d}); });
-    
+
     console.log(xFields);
 
     var yFields = Object.keys(barData[0].value).map(function (d) { return ({"value":d, "label":d}); });
 
-    // return the title, the dropdown menus, and the barplot with axes  
+    // return the title, the dropdown menus, and the barplot with axes
   	return(
       React.createElement( React.Fragment, null,
         React.createElement( 'header', null,
@@ -376,9 +381,9 @@
         ),
         React.createElement( 'h1', null, " NYDOCCS Under Custody Data " )
   			),
-        
+
         React.createElement( 'h1', { ref: function (d) { return SVG(d); } }, " "),
-  			
+
         React.createElement( 'div', { className: 'menu-container' },
         React.createElement( 'span', { className: "dropdown-label" }, "X"),
         React.createElement( ReactDropdown__default['default'], {
@@ -397,17 +402,17 @@
             return setYAttribute(value);
     } })
         ),
-        
+
   			React.createElement( 'div', { id: 'radio_sort', ref: function (d) { return Bar(d, barData, yAttribute, xAttribute); }, class: "control-group" },
-          React.createElement( 'label', { class: "control control-radio" }, "Sort by Height ", React.createElement( 'input', {  className: 'radio', type: "radio", value: "height", name: "sort" }),  
+          React.createElement( 'label', { class: "control control-radio" }, "Sort by Height ", React.createElement( 'input', {  className: 'radio', type: "radio", value: "height", name: "sort" }),
               React.createElement( 'div', { class: "control_indicator" })
           ),
-          React.createElement( 'label', { class: "control control-radio" }, "Sort by X Value ", React.createElement( 'input', { className: 'radio', type: "radio", value: "x", name: "sort" }),  
+          React.createElement( 'label', { class: "control control-radio" }, "Sort by X Value ", React.createElement( 'input', { className: 'radio', type: "radio", value: "x", name: "sort" }),
               React.createElement( 'div', { class: "control_indicator" })
           )
       ),
-        
-        
+
+
       React.createElement( Table, { barData: barData, yAttribute: yAttribute, xAttribute: xAttribute })
   		)
   	);
