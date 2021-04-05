9a6dd6f75d0a89cccc78c2b9fdcafaa130ac7aa4
Xiaoyu Su <68837507+Xiaoyu-Su-20@users.noreply.github.com>
2021-04-05 16:47:10 -0400

    Delete Dropdown.js

--- Dropdown.js
+++
@@ -1,9 +0,0 @@
-export const Dropdown = ({ options, id, selectedValue, onSelectedValueChange }) => (
-  <select id={id} onChange={event => onSelectedValueChange(event.target.value)}>
-    {options.map(({ value, label }) => (
-      <option value={value} selected={value === selectedValue}>
-        {label}
-      </option>
-    ))}
-  </select>
-);
