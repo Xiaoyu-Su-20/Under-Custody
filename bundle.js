3de4c61df40c723201ab8d159589242028613375
Evan Misshula <emisshula@jjay.cuny.edu>
2021-04-06 16:38:06 -0400

    updates bundle

--- bundle.js
+++ bundle.js
@@ -183,7 +183,7 @@
             }else {
               tooltip
               .html(
-                ("<div>" + (toTitle(xAttribute)) + ": " + (d.key) + "</div>\n              <div>" + (toTitle(yAttribute)) + ": " + (formatNumber(d.value[yAttribute].toFixed(0))) + "</div>\n              <div>" + ('Count ') + (d.key) + ": " + (formatNumber(d.value.amount.toFixed(0))) + "</div>")
+                ("<div>" + (toTitle(xAttribute)) + ": " + (d.key) + "</div>\n              <div>" + (toTitle(yAttribute)) + ": " + (formatNumber(d.value[yAttribute].toFixed(2))) + "</div>\n              <div>" + ('Count ') + (d.key) + ": " + (formatNumber(d.value.amount.toFixed(0))) + "</div>")
               )
               .style('visibility', 'visible');
             d3.select(this).style("opacity", 0.7);
